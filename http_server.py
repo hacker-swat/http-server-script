b413dfec2133e446ff90aa735b4793c6c6205d55
hackerswat <80219749+hackerswat@users.noreply.github.com>
2023-01-13 15:56:18 +0200

    Update http_server.py

--- http_server.py
+++ http_server.py
@@ -9,7 +9,9 @@ def run_server(host, port, protocol, certificate, key):
     server_address = (host, port)
     if protocol == "https":
         httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
-        httpd.socket = ssl.wrap_socket(httpd.socket, certfile=certificate, keyfile=key, server_side=True)
+        context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        context.load_cert_chain(certfile=certificate, keyfile=key)
+        httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
         print(f"Serving HTTPS on {host}:{port}...")
         listdirs(host, port, protocol)
     else:
@@ -19,10 +21,6 @@ def run_server(host, port, protocol, certificate, key):
     httpd.serve_forever()
     
 def do_checks(host, port, protocol, certificate, key,generate_ssl):
-    if generate_ssl==True:
-        print("Not available (future update) in the meantime run the command")
-        print("openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365")
-
     def linNetInfo(host):
         interface_names = []
         for index, name in socket.if_nameindex():
@@ -53,14 +51,23 @@ def do_checks(host, port, protocol, certificate, key,generate_ssl):
     if port not in range(1, 65535):
         raise ValueError("invalid port")
     
+    if generate_ssl == "True":
+        generatessl(host)
+    
     protocol = protocol.lower()
     if protocol != "http" and protocol != "https": 
         raise ValueError("invalid protocol")
     if protocol == "https":
         if certificate == "":
-            raise ValueError("invalid certificate")
-        if key == "":
-            raise ValueError("invalid key")
+            if os.path.exists(host+".crt"):
+                certificate = str("./" + host+".crt")
+            else:
+                raise ValueError("invalid certificate")
+        if key == "":    
+            if os.path.exists(host+".key"):
+                    key = str("./" + host +".key")
+            else:
+                raise ValueError("invalid key")
     return host, port, protocol, certificate, key
 
 def listdirs(host, port, protocol):
@@ -92,23 +99,52 @@ def listdirs(host, port, protocol):
     | |__| (_| | |_| | |_| | (_) | | | |  _\__ \__ \ | | | | \__ \ | | | | |  | | | |  __/ | |  __/                      
      \____\__,_|\__,_|\__|_|\___/|_| |_| (_)___/___/_| |_| |_|___/ |_|_| |_|  |_| |_|\___|_|  \___|
         ''')
+    print("\n")
 
+def generatessl(host):
+    try:
+        from OpenSSL import crypto
+    except:
+        raise ValueError("\nplease install openssl library\npip install pyopenssl")
+    key = crypto.PKey()
+    key.generate_key(crypto.TYPE_RSA, 2048)
+    cert = crypto.X509()
+    cert.get_subject().C = "NA"
+    cert.get_subject().ST = "NA"
+    cert.get_subject().L = "NA"
+    cert.get_subject().O = "NA"
+    cert.get_subject().OU = "NA"
+    cert.get_subject().CN = host
+    cert.set_serial_number(1000)
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(315360000)
+    cert.set_issuer(cert.get_subject())
+    cert.set_pubkey(key)
+    cert.sign(key, 'sha256')
+    open("{}.key".format(host), "wb").write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
+    open("{}.crt".format(host), "wb").write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+    certificate = "{}.crt".format(host)
+    key = "{}.key".format(host)
+    print("Certificate generated")   
 
 if __name__ == "__main__":
     try:
-        parser = argparse.ArgumentParser(description="Run a HTTP file server")
-        parser.add_argument("--host", type=str, default="", help="hostname/ip to listen on")
+        parser = argparse.ArgumentParser(description="Run a simple HTTP file server")
+        parser.add_argument("--host", type=str, default="", help="hostname to listen on")
         parser.add_argument("--port", type=int, default=8000, help="port number to listen on (default set to 8000)")
         parser.add_argument("--protocol", type=str,default="http", help="Protocol to use (http or https)")
         parser.add_argument("--certificate", type=str, default="", help="https certificate.pem")
         parser.add_argument("--key", type=str, default="", help="https private key.pem")
-        parser.add_argument("--generate-ssl" ,default="False" ,action='store_true',help="generate https server certificate")
+        parser.add_argument("--generate-ssl" ,action='store_true',help="generate https server certificate")
         args = parser.parse_args()
         
 
-        i = do_checks(args.host, args.port, args.protocol,args.certificate,args.key,args.generate_ssl)
+        i = do_checks(args.host, args.port, args.protocol,args.certificate,args.key,str(args.generate_ssl))
         
+
         run_server(i[0], i[1], i[2],i[3],i[4])
+        print(i)
+
 
     except KeyboardInterrupt:
         print ("\nShutdown requested...exiting")
