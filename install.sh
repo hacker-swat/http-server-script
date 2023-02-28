2f4323ad9e1550777eea8f1ea02793010f0c3521
hackerswat <80219749+hackerswat@users.noreply.github.com>
2023-02-28 21:50:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ cp ./http_server.py ./README.md $script_location
 
 py=$(which python3)
 
-echo -e "#!/bin/bash \n$py $script_location/http_server.py $@" > "/usr/local/bin/httpserver"
+echo -e "#!/bin/bash \n$py $script_location/http_server.py \$@" > "/usr/local/bin/httpserver"
 
 chmod 755  /usr/local/bin/httpserver
 
